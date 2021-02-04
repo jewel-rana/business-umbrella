50978715505785cd687cf90d2d694548086272c4
Jewel Rana <jewelrana.dev@gmail.com>
2021-02-04 10:18:36 +0600

    Mode & migration creation done

---
+++ database/migrations/2021_02_04_041244_create_porposals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePorposalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('porposals', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('file_name');
+            $table->string('file_extension');
+            $table->string('file_size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('porposals');
+    }
+}
