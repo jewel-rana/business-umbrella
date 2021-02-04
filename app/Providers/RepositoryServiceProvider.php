d37d5d0a4fc61a0421791726b9df4081f7d8bfa3
Jewel Rana <jewelrana.dev@gmail.com>
2021-02-04 12:17:37 +0600

    Repository service provider created

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\BaseRepository;
+use App\Repositories\CompanyRepository;
+use App\Repositories\Interfaces\BaseRepositoryInterface;
+use App\Repositories\Interfaces\CompanyRepositoryInterface;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(BaseRepositoryInterface::class, BaseRepository::class);
+        $this->app->bind(CompanyRepositoryInterface::class, CompanyRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
