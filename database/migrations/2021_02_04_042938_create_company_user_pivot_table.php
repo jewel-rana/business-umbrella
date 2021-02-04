7a0e13cc1ded625a821532bc13c328413a8887e7
Jewel Rana <jewelrana.dev@gmail.com>
2021-02-04 10:31:26 +0600

    Pivot Table migration done

---
+++ database/migrations/2021_02_04_042938_create_company_user_pivot_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyUserPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_user', function (Blueprint $table) {
+            $table->bigInteger('company_id');
+            $table->bigInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_user_pivot');
+    }
+}
